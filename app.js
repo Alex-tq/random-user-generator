326a55b4e68949cea0f9f8c9e7702f7604ad48da
unknown <alextq3036@gmail.com>
2022-01-22 00:45:18 -0500

    Added to fuctionaliity to get a random user when the button is clicked

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ const app = Vue.createApp({
         "https://www.newamericanjackets.com/wp-content/uploads/2021/05/Johnny-Cage-Mortal-Kombat-X-Hooded-Vest.jpg",
     };
   },
+  methods: {
+    async getUser() {
+      const res = await fetch("https://randomuser.me/api");
+      const { results } = await res.json();
+
+      console.log(results[0]);
+      this.firstName = results[0].name.first;
+      this.lastName = results[0].name.last;
+      this.email = results[0].email;
+      this.gender = results[0].gender;
+      this.picture = results[0].picture.large;
+    },
+  },
 });
 
 app.mount("#app");
